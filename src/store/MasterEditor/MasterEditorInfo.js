9fdd233a662abfb431b9293c7d5e18d3df53fff5
maiordom <maiordom@yandex.ru>
2017-04-09 00:39:46 +0300

    make CreateMasterQuery

--- src/store/MasterEditor/MasterEditorInfo.js
+++ src/store/MasterEditor/MasterEditorInfo.js
@@ -1,22 +1,39 @@
+/* @flow */
+
+type PhotosModel = {
+  items: Array<string>,
+  limit: number,
+  queryParam: string,
+  queryType: string,
+};
+
 const workPhotos = () => ({
   items: [],
   limit: 15,
-});
+  queryParam: 'work_photos',
+  queryType: 'array',
+}: PhotosModel);
 
 const passportPhotos = () => ({
   items: [],
   limit: 1,
-});
+  queryParam: 'passport',
+  queryType: 'string',
+}: PhotosModel);
 
 const certificatePhotos = () => ({
   items: [],
   limit: 10,
-});
+  queryParam: 'certificates',
+  queryType: 'array',
+}: PhotosModel);
 
 const personalPhotos = () => ({
   items: [],
   limit: 5,
-});
+  queryParam: 'master_photos',
+  queryType: 'array',
+}: PhotosModel);
 
 export default {
   certificatePhotos,
